5bc1528764c9e4a617ab2da5b73af4c48f23f26b
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-07-31 18:30:30 +0100

    Pt 3

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,7 +1,21 @@
-import { setBackgroundColor } from "./roomsUtils.js";
+import { setBackgroundColor, setMapColliders } from "./roomsUtils.js";
 
 export function room1(k, roomData) {
     setBackgroundColor(k, "#a2aed5");
 
+    k.camScale(4);
+    k.camPos(170, 100);
+    k.setGravity(1000)
+
     const roomLayers = roomData.layers;
+
+    const map = k.add([k.pos(0, 0), k.sprite("room1"),])
+    const colliders = []
+    for (const layer of roomLayers) {
+       if (layer.name === "colliders") {
+            colliders.push(...layer.objects);
+       }
+    }
+    
+    setMapColliders(k, map, colliders);
 }
